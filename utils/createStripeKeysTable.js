4ebe0c28241258cb568512906dc11c000a44b974
Vijay Lakshkar <vijaylakshkar.sst@gmail.com>
2025-11-24 12:53:52 +0530

    Update createStripeKeysTable.js

--- utils/createStripeKeysTable.js
+++ utils/createStripeKeysTable.js
@@ -15,8 +15,8 @@ CREATE TABLE IF NOT EXISTS stripe_keys (
 const seedQuery = `
 INSERT INTO stripe_keys (environment, publishable_key, secret_key, webhook_secret)
 VALUES
-    ('test', 'pk_test_51SIgofRy2tSjrfvAlRFYM801gPuen8zEvsIPam9YuH8AnOzDgMg8SiHOyelFVOJekCvnZBr4BYhUGoCgP8FgcuRd00HVdew2RE', 'sk_test_51SIgofRy2tSjrfvAymU2pILf6q13pMKJF0rDHVpzyiRCLNbgcDQa4RnutPOrDQpJkL4Bj72S8NRgBQEONbqpXLJd00X6mO6nj4', 'whsec_DBlIqCdo4tGkozt5pOVMFGJ0nGjrNkJH'),
-    ('production', 'pk_live_51SIgoNRrFdjZS8bFjix2a7RmiRJb6AVkUTGBUPQl8iLyhaEHLfBI1qJT6u8hh8RfR3VnbH7OPEjt2W4r3F6prNUJ00a8lUSn6D', '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', 'whsec_live_xxxxx')
+    ('test', 'hjk', 'hjk', 'whsec_DBlIqCdo4fghtGkozt5pOVMFGJ0nGjrNkJH'),
+    ('production', 'hjkjh', 'hjk', 'whsec_live_xxxxx')
 ON CONFLICT (environment) DO NOTHING;
 `;
 
